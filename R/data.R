18bae7581286fe0746bc11ec2a91ee9740875e61
oliviaxu17 <sxu17@amherst.edu>
2017-02-26 17:03:29 -0500

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,87 @@
+#' "Roughing It"
+#'
+#' A dataset containing the full text of "Roughing It" by Mark Twain. The UTF-8 plain text was
+#' sourced from the gutenberg.org website. Each line represents a 
+#' line in the original text.
+#'
+#' @usage roughingit
+#'
+#' @format A character vector with 13981 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/3177/3177.txt}
+#' 
+"roughingit"
+
+#' "Innocents Abroad"
+#'
+#' A dataset containing the full text of "Innocents Abroad" by Mark Twain. 
+#' The UTF-8 plain text was sourced from the gutenberg.org website. 
+#' Each line represents a line in the original text
+#'
+#' @usage innocentsabroad
+#'
+#' @format A character vector with 16161 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/3176/3176-0.txt}
+#' 
+"innocentsabroad"
+
+#' "The Adventures of Tom Sawyer"
+#'
+#' A dataset containing the full text of "The Adventures of Tom
+#' Sawer" by Mark Twain. The UTF-8 plain text was
+#' sourced from the gutenberg.org 
+#' website. Each line represents a line in the original text.
+#'
+#' @usage tomsawyer
+#'
+#' @format A character vector with 6374 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/74/74-0.txt}
+#' 
+"tomsawyer"
+
+#' "The Adventures of Huckleberry Finn"
+#'
+#' A dataset containing the full texts of "The Adventures of  
+#' Huckleberry Finn" by Mark Twain. The UTF-8 plain text was
+#' sourced from the gutenberg.org website. 
+#' Each line represents a line in the original text.
+#'
+#' @usage huckfinn
+#'
+#' @format A character vector with 9082 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/76/76-0.txt}
+#' 
+"huckfinn"
+
+#' "A Connecticut Yankee in King Arthur's Court"
+#'
+#' A dataset containing the full texts of "A Connecticut Yankee in  
+#' King Arthur's Court" by Mark Twain. The UTF-8 plain text was
+#' sourced from the gutenberg.org website. 
+#' Each line represents a line in the original text.
+#'
+#' @usage connyankee
+#'
+#' @format A character vector with 10521 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/86/86-0.txt}
+#' 
+"connyankee"
+
+#' "Pudd'nhead Wilson"
+#'
+#' A dataset containing the full texts of "Pudd'nhead 
+#' Wilson" by Mark Twain. The UTF-8 plain text was
+#' sourced from the gutenberg.org website. 
+#' Each line represents a line in the original text.
+#'
+#' @usage pudd
+#'
+#' @format A character vector with 4513 elements
+#'
+#' @source \url{http://www.gutenberg.org/files/102/102-0.txt}
+#' 
+"pudd"
